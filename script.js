6f96e5788055ecb457a92f9415027581e4358188
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-26 13:49:33 +0200

    Changed the code so that it can work on different objects

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const car2 = {
   miles: 1800,
 };
 
-var taxi = {
+const taxi = {
   make: "Webville Motors",
   model: "Taxi",
   year: 1955,
@@ -19,14 +19,54 @@ var taxi = {
   mileage: 281341,
 };
 
-function checkCar(carSpecs) {
-  return carSpecs.builtIn < 1960 && carSpecs.miles < 10000;
+const cadi = {
+  make: "GM",
+  model: "Cadillac",
+  year: 1955,
+  color: "tan",
+  passengers: 5,
+  convertible: false,
+  mileage: 12892,
+};
+
+const fiat = {
+  make: "Fiat",
+  model: "500",
+  year: 1957,
+  color: "Medium Blue",
+  passengers: 2,
+  convertible: false,
+  mileage: 88000,
+};
+
+const chevy = {
+  make: "Chevy",
+  model: "Bel Air",
+  year: 1957,
+  color: "red",
+  passengers: 2,
+  convertible: false,
+  mileage: 1021,
+};
+
+function checkCar(car) {
+  //   return carSpecs.builtIn < 1960 && carSpecs.miles < 10000;
+  if (car.year < 1960 && car.mileage < 10000) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-let worthAlook = checkCar(taxi);
-
-if (worthAlook) {
-  console.log(`You gotta check out this ${taxi.make}, ${taxi.model}`);
-} else {
-  console.log(`You should really pass on the ${taxi.make}, ${taxi.model}`);
+function worthAlook(car) {
+  if (checkCar(car)) {
+    console.log(`You gotta check out this ${car.make}, ${car.model}`);
+  } else {
+    console.log(`You should really pass on the ${car.make}, ${car.model}`);
+  }
 }
+
+worthAlook(taxi);
+worthAlook(cadi);
+worthAlook(fiat);
+worthAlook(chevy);
