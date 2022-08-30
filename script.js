84ca71d3d32fb19b488936f423d0a2cb92d9c234
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-30 11:20:27 +0200

    Auto-o-matic exercise

--- script.js
+++ script.js
@@ -1,72 +1,107 @@
 "Use strict";
 
-const car = {
-  builtIn: 1954,
-  miles: 8000,
-};
-const car2 = {
-  builtIn: 1978,
-  miles: 1800,
-};
+// const car = {
+//   builtIn: 1954,
+//   miles: 8000,
+// };
+// const car2 = {
+//   builtIn: 1978,
+//   miles: 1800,
+// };
 
-const taxi = {
-  make: "Webville Motors",
-  model: "Taxi",
-  year: 1955,
-  color: "yellow",
-  passengers: 4,
-  convertible: false,
-  mileage: 281341,
-};
+// const taxi = {
+//   make: "Webville Motors",
+//   model: "Taxi",
+//   year: 1955,
+//   color: "yellow",
+//   passengers: 4,
+//   convertible: false,
+//   mileage: 281341,
+// };
 
-const cadi = {
-  make: "GM",
-  model: "Cadillac",
-  year: 1955,
-  color: "tan",
-  passengers: 5,
-  convertible: false,
-  mileage: 12892,
-};
+// const cadi = {
+//   make: "GM",
+//   model: "Cadillac",
+//   year: 1955,
+//   color: "tan",
+//   passengers: 5,
+//   convertible: false,
+//   mileage: 12892,
+// };
 
-const fiat = {
-  make: "Fiat",
-  model: "500",
-  year: 1957,
-  color: "Medium Blue",
-  passengers: 2,
-  convertible: false,
-  mileage: 88000,
-};
+// const fiat = {
+//   make: "Fiat",
+//   model: "500",
+//   year: 1957,
+//   color: "Medium Blue",
+//   passengers: 2,
+//   convertible: false,
+//   mileage: 88000,
+// };
 
-const chevy = {
-  make: "Chevy",
-  model: "Bel Air",
-  year: 1957,
-  color: "red",
-  passengers: 2,
-  convertible: false,
-  mileage: 1021,
-};
+// const chevy = {
+//   make: "Chevy",
+//   model: "Bel Air",
+//   year: 1957,
+//   color: "red",
+//   passengers: 2,
+//   convertible: false,
+//   mileage: 1021,
+// };
 
-function checkCar(car) {
-  //   return carSpecs.builtIn < 1960 && carSpecs.miles < 10000;
-  if (car.year < 1960 && car.mileage < 10000) {
-    return true;
-  } else {
-    return false;
-  }
+// function checkCar(car) {
+//   //   return carSpecs.builtIn < 1960 && carSpecs.miles < 10000;
+//   if (car.year < 1960 && car.mileage < 10000) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function worthAlook(car) {
+//   if (checkCar(car)) {
+//     console.log(`You gotta check out this ${car.make}, ${car.model}`);
+//   } else {
+//     console.log(`You should really pass on the ${car.make}, ${car.model}`);
+//   }
+// }
+
+// worthAlook(taxi);
+// worthAlook(cadi);
+// worthAlook(fiat);
+// worthAlook(chevy);
+
+// =======================
+// AUTO-O-MATIC
+// PAGE 195
+
+function makeCar() {
+  const makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
+  const models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
+  const years = [1955, 1957, 1948, 1954, 1961];
+  const colors = ["red", "blue", "tan", "yellow", "white"];
+  const convertible = [true, false];
+  const rand1 = Math.floor(Math.random() * makes.length);
+  const rand2 = Math.floor(Math.random() * models.length);
+  const rand3 = Math.floor(Math.random() * years.length);
+  const rand4 = Math.floor(Math.random() * colors.length);
+  const rand5 = Math.floor(Math.random() * 5) + 1;
+  const rand6 = Math.floor(Math.random() * 2);
+  const car = {
+    make: makes[rand1],
+    model: models[rand2],
+    year: years[rand3],
+    color: colors[rand4],
+    passengers: rand5,
+    convertible: convertible[rand6],
+    mileage: 0,
+  };
+  return car;
 }
-
-function worthAlook(car) {
-  if (checkCar(car)) {
-    console.log(`You gotta check out this ${car.make}, ${car.model}`);
-  } else {
-    console.log(`You should really pass on the ${car.make}, ${car.model}`);
-  }
+function displayCar(car) {
+  console.log(
+    "Your new car is a " + car.year + " " + car.make + " " + car.model
+  );
 }
-
-worthAlook(taxi);
-worthAlook(cadi);
-worthAlook(fiat);
-worthAlook(chevy);
+const carToSell = makeCar();
+displayCar(carToSell);
